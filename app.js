6c55abeb49a1054d35f887a7e250ecb62d806b4f
RYAN <ryancpotts@gmail.com>
2024-02-29 17:44:43 -0600

    app.js is done, do dom manipulation now

--- app.js
+++ app.js
@@ -9,110 +9,120 @@ let seattleObject = {
     avgSale: 6.3,
     cookiesSalesArray: [],
     generateCookieSales: function () {
-        for (let i=0; i < hours.length; i++) {
+        for (let i = 0; i < hours.length; i++) {
 
-        let randCustomers= Math.floor( Math.random() * (this.maxCustomers-this.minCustomers+1) + this.minCustomers);
-        
-        let cookiesThisHour = Math.floor( randCustomers * this.avgSale);
-    
-        this.cookiesSalesArray.push(cookiesThisHour)
-    
-        this.console.log(seattleObject.cookiesSalesArray)
+            let randCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+
+            let cookiesThisHour = Math.floor(randCustomers * this.avgSale);
+
+            this.cookiesSalesArray.push(cookiesThisHour)
+
+            console.log(this.cookiesSalesArray)
+        }
     }
-    
+}
+
 
 
 let tokyoObject = {
-    name: Tokyo,
-    phone: 222-222-2222,
+    name: "Tokyo",
+    phone: "222 - 222 - 2222",
     address: '1 Main St. #1, Tokyo, Japan 035-894',
     minCustomers: 3,
     maxCustomers: 24,
     avgSale: 1.2,
     cookiesSalesArray: [],
     generateCookieSales: function () {
-        for (let i=0; i < hours.length; i++) {
+        for (let i = 0; i < hours.length; i++) {
 
-        let randCustomers= Math.floor( Math.random() * (this.maxCustomers-this.minCustomers+1) + this.minCustomers);
-        
-        let cookiesThisHour = Math.floor( randCustomers * this.avgSale);
-    
-        this.cookiesSalesArray.push(cookiesThisHour)
-    
-        this.console.log(tokyoObject.cookiesSalesArray)
+            let randCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+
+            let cookiesThisHour = Math.floor(randCustomers * this.avgSale);
+
+            this.cookiesSalesArray.push(cookiesThisHour)
+
+            console.log(this.cookiesSalesArray)
+        }
     }
-    
-
-// }
-
-let dubaiObject = {
-    name: Dubai,
-    phone: 333-333-3333,
-    address: 'PO BOX 123, Dubai, UAE',
-    minCustomers: 11,
-    maxCustomers: 38,
-    avgSale: 3.7,
-    cookiesSalesArray: [],
-    generateCookieSales: function () {
-        for (let i=0; i < hours.length; i++) {
-
-        let randCustomers= Math.floor( Math.random() * (this.maxCustomers-this.minCustomers+1) + this.minCustomers);
-        
-        let cookiesThisHour = Math.floor( randCustomers * this.avgSale);
-    
-        this.cookiesSalesArray.push(cookiesThisHour)
-    
-        this.console.log(dubaiObject.cookiesSalesArray)
-    }
-
-let parisObject = {
-    name: Paris,
-    phone: 444-444-4444,
-    address: 'PO Box 789, Paris, France',
-    minCustomers: 20,
-    maxCustomers: 38,
-    avgSale: 2.3,
-    cookiesSalesArray: [],
-    generateCookieSales: function () {
-        for (let i=0; i < hours.length; i++) {
-
-        let randCustomers= Math.floor( Math.random() * (this.maxCustomers-this.minCustomers+1) + this.minCustomers);
-        
-        let cookiesThisHour = Math.floor( randCustomers * this.avgSale);
-    
-        this.cookiesSalesArray.push(cookiesThisHour)
-    
-        this.console.log(parisObject.cookiesSalesArray)
-    }
-
-let limaObject = {
-    name: Lima,
-    phone: 555-555-5555,
-    address: 'PO Box 311, Lima Peru',
-    minCustomers: 2,
-    maxCustomers: 16,
-    avgSale: 4.6,
-    cookiesSalesArray: [],
-    generateCookieSales: function () {
-        for (let i=0; i < hours.length; i++) {
-
-        let randCustomers= Math.floor( Math.random() * (this.maxCustomers-this.minCustomers+1) + this.minCustomers);
-        
-        let cookiesThisHour = Math.floor( randCustomers * this.avgSale);
-    
-        this.cookiesSalesArray.push(cookiesThisHour)
-    
-        this.console.log(tokyoObject.cookiesSalesArray)
-    }
+}
 
 
-seattleObject.generateCookieSales()
-console.log(seattleObject.cookiesSalesArray)
-tokyoObject.generateCookieSales()
-console.log(tokyoObject.cookiesSalesArray)
-dubaiObject.generateCookieSales()
-console.log(dubaiObject.cookiesSalesArray)
-parisObject.generateCookieSales()
-console.log(parisObject.cookiesSalesArray)
-limaObject.generateCookieSales()
-console.log(limaObject.cookiesSalesArray)
+        // }
+
+        let dubaiObject = {
+            name: "Dubai",
+            phone: "333 - 333 - 3333",
+            address: 'PO BOX 123, Dubai, UAE',
+            minCustomers: 11,
+            maxCustomers: 38,
+            avgSale: 3.7,
+            cookiesSalesArray: [],
+            generateCookieSales: function () {
+                for (let i = 0; i < hours.length; i++) {
+
+                    let randCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+
+                    let cookiesThisHour = Math.floor(randCustomers * this.avgSale);
+
+                    this.cookiesSalesArray.push(cookiesThisHour)
+
+                    console.log(this.cookiesSalesArray)
+                }
+            }
+        }
+
+                let parisObject = {
+                    name: "Paris",
+                    phone: "444 - 444 - 4444",
+                    address: 'PO Box 789, Paris, France',
+                    minCustomers: 20,
+                    maxCustomers: 38,
+                    avgSale: 2.3,
+                    cookiesSalesArray: [],
+                    generateCookieSales: function () {
+                        for (let i = 0; i < hours.length; i++) {
+
+                            let randCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+
+                            let cookiesThisHour = Math.floor(randCustomers * this.avgSale);
+
+                            this.cookiesSalesArray.push(cookiesThisHour)
+
+                            console.log(this.cookiesSalesArray)
+                        }
+                    }
+                }
+
+                        let limaObject = {
+                            name: "Lima",
+                            phone: "555 - 555 - 5555",
+                            address: 'PO Box 311, Lima Peru',
+                            minCustomers: 2,
+                            maxCustomers: 16,
+                            avgSale: 4.6,
+                            cookiesSalesArray: [],
+                            generateCookieSales: function () {
+                                for (let i = 0; i < hours.length; i++) {
+
+                                    let randCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) + this.minCustomers);
+
+                                    let cookiesThisHour = Math.floor(randCustomers * this.avgSale);
+
+                                    this.cookiesSalesArray.push(cookiesThisHour)
+
+                                    console.log(this.cookiesSalesArray)
+                                }
+                            }
+                        }
+
+
+                                seattleObject.generateCookieSales()
+                                console.log(seattleObject.cookiesSalesArray)
+                                tokyoObject.generateCookieSales()
+                                console.log(tokyoObject.cookiesSalesArray)
+                                dubaiObject.generateCookieSales()
+                                console.log(dubaiObject.cookiesSalesArray)
+                                parisObject.generateCookieSales()
+                                console.log(parisObject.cookiesSalesArray)
+                                limaObject.generateCookieSales()
+                                console.log(limaObject.cookiesSalesArray)
